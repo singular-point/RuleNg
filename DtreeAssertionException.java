d27bca4dece6853715bf75b164dba2ab204ddbc9
Rainyn <2695792010@qq.com>
2021-06-23 19:13:00 +0800

    Create DtreeAssertionException.java

---
+++ DtreeAssertionException.java
@@ -0,0 +1,15 @@
+public class DtreeAssertionException extends RuntimeException {
+
+    public DtreeAssertionException(String s) {
+        super(s);
+    }
+
+    public DtreeAssertionException() {
+        super();
+    }
+
+    public DtreeAssertionException(String s, Throwable cause) {
+        super(s, cause);
+    }
+
+}
