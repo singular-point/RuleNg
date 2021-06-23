794ef738373b8be7cc2068de8bdaeeee65befa88
Rainyn <2695792010@qq.com>
2021-06-23 19:18:42 +0800

    Create Student.java

---
+++ demo/Student.java
@@ -0,0 +1,18 @@
+import lombok.Data;
+
+/**
+ * @Author yuanjingkun
+ * @create 2020/6/13 10:34
+ */
+@Data
+public class Student {
+
+    Integer age;
+
+    String gender;
+
+    Integer grade;
+
+    Integer classNum;
+
+}
